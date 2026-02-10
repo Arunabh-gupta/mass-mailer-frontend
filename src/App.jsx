33683b6bdd0513ddc337bf71540b5ed9b61fac2c
Arunabh Gupta <arundevs765@gmail.com>
2026-02-11 03:42:39 +0530

    tailwind setup is still pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
-
 function App() {
-
   return (
-    <div>
-      Hello world
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-8xl font-bold text-blue-600">
+        Tailwind + PostCSS is working 🚀
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
