391038c1caf1067b857e452e1df682bcce05ce50
Arunabh Gupta <arundevs765@gmail.com>
2026-02-14 03:15:50 +0530

    tailwind setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-8xl font-bold text-blue-600">
+    <div className="min-h-screen flex items-center justify-center bg-grey-100">
+      <h1 className="text-4xl font-bold text-red-600">
         Tailwind + PostCSS is working 🚀
       </h1>
     </div>
